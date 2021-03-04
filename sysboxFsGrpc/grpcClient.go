4da3f89eb8e5c8b7bc10207f5667423073e23d35
Cesar Talledo <ctalledo@nestybox.com>
2021-03-04 18:57:40 +0000

    Add userns info to sysbox-run -> sysbox-fs grpc.
    Signed-off-by: Cesar Talledo <ctalledo@nestybox.com>

--- sysboxFsGrpc/grpcClient.go
+++ sysboxFsGrpc/grpcClient.go
@@ -30,6 +30,7 @@ import (
 // Container info passed by the client to the server across the grpc channel
 type ContainerData struct {
 	Id            string
+	Userns        string
 	InitPid       int32
 	Hostname      string
 	Ctime         time.Time
@@ -72,6 +73,7 @@ func containerDataToPbData(data *ContainerData) (*pb.ContainerData, error) {
 
 	return &pb.ContainerData{
 		Id:            data.Id,
+		Userns:        data.Userns,
 		InitPid:       data.InitPid,
 		Ctime:         pbTime,
 		UidFirst:      data.UidFirst,
@@ -91,6 +93,7 @@ func pbDatatoContainerData(pbdata *pb.ContainerData) (*ContainerData, error) {
 
 	return &ContainerData{
 		Id:            pbdata.Id,
+		Userns:        pbdata.Userns,
 		InitPid:       pbdata.InitPid,
 		Ctime:         cTime,
 		UidFirst:      pbdata.UidFirst,
